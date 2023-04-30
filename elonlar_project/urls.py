83f605389b4d154f050c1e712c5407df783f6dcf
s-muzaffarbek <muzaffarbeksaidahmedov@gmail.com>
2023-04-30 15:31:51 +0500

    changes

--- elonlar_project/urls.py
+++ elonlar_project/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     *i18n_patterns(*urlpatterns, prefix_default_language=False),
